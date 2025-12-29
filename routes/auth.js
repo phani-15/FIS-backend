954ddbaef3777efa77a7b7a35a19bbd95104ff0f
Narendra-619 <kknarendra619@gmail.com>
2025-12-29 21:28:57 +0530

    trail commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,10 @@
 import express from "express"
 import {check} from "express-validator"
-import {login,register,sendmail,adminlogin,hodlogin,hodregister} from "../controllers/auth.js"
+import {login,register,adminlogin,hodlogin,hodregister} from "../controllers/auth.js"
 import { defaultArgs } from "puppeteer";
-import { forgotPassword } from "../controllers/auth.js";
-import { resetPassword } from "../controllers/auth.js";
-import { verifyOTP } from "../controllers/auth.js"; 
-import { iqacRegister,Iqaclogin } from "../controllers/auth.js";
+import { forgotPassword,resetPassword,verifyOTP,sendmail } from "../utils/nodemailer.js";
+import { upload } from "../utils/multer.js";
+import { iqacRegister,ofclogin } from "../controllers/auth.js";
 const router=express.Router()
 
 //handlle Register
@@ -14,7 +13,7 @@ router.post("/hodregister",hodregister);
 router.post("/ofcregister",iqacRegister);
 router.post("/login",login)
 router.post("/admin",adminlogin)
-router.post("/iqac",Iqaclogin)
+router.post("/iqac",ofclogin)
 router.post("/hod",hodlogin)
 
 //handle sending mail

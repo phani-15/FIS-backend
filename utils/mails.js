c172ea9414754dbcd956d7365927ca2edcc3e401
phani-15 <phanipolavarapu15@gmail.com>
2026-01-18 10:26:16 +0530

    nothing

--- utils/mails.js
+++ utils/mails.js
@@ -2,7 +2,7 @@ export const ofcMails = {
   "Vice Principal": "slayking619@gmail.com",
   "Principal": "studentcse123456789@gmail.com",
   "IQAC Coordinator": "iqac@mail.com",
-  "IQAC Director": "iqacdirecctor@gamil.com",
+  "IQAC Director": "phanipolavarapu15@gmail.com",
   "R&D Director": "randddirec@mail.com"
 };
 
